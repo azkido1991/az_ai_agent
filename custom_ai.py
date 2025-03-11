943a455f5764c6640074fd7f84ad91152fcd0191
azkido1991 <chong.wailun@armourzero.com>
2025-03-11 19:10:28 +0800

    Add files via upload

---
+++ custom_ai.py
@@ -0,0 +1,29 @@
+
+import asyncio
+import os
+from dataclasses import dataclass
+from typing import Any
+
+import logfire
+from devtools import debug
+from httpx import AsyncClient
+
+from pydantic_ai import Agent, ModelRetry, RunContext
+from pydantic_ai.models.openai import OpenAIModel
+from pydantic_ai.providers.openai import OpenAIProvider
+
+
+llm = 'deepseek/deepseek-r1-zero:free'
+api = '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'
+model = OpenAIModel(llm, base_url = 'https://openrouter.ai/api/v1', api_key=api)
+agent = Agent(model)
+
+async def main():
+    async with AsyncClient() as client:
+        result = await agent.run(
+            'Tell me a funny story'
+        )
+        print('Response:', result.data)
+
+if __name__ == '__main__':
+    asyncio.run(main())
