65788bca296c10e6f450b3edfd817fb5af00dd8d
azkido1991 <chong.wailun@armourzero.com>
2025-03-11 21:00:48 +0800

    Update custom_ai.py

--- custom_ai.py
+++ custom_ai.py
@@ -14,7 +14,7 @@ from pydantic_ai.providers.openai import OpenAIProvider
 
 
 llm = 'deepseek/deepseek-r1-zero:free'
-api = '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'
+api = 'sk-or-v1-1ef885c6827ac8356284ca05885e99885939d7cd2f087f9c860f20dd8350f4fb33'
 model = OpenAIModel(llm, base_url = 'https://openrouter.ai/api/v1', api_key=api)
 agent = Agent(model)
 
